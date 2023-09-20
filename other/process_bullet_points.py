28d78df5bca584d49d8c4383c4adaf91678610c7
Eric Zhou <ericsuper@outlook.com>
2023-09-20 00:50:12 -0400

    Finished chatbot, evolutionary reinforcement learning, and typing bot project details

--- other/process_bullet_points.py
+++ other/process_bullet_points.py
@@ -1,10 +1,9 @@
 s = """
-• Implemented various statistical methods and advanced machine learning models including Bollinger Bands, Support Vector Machines (SVMs), and Long Short-Term Memory (LSTM) networks to analyze and predict stock market trends
- • Utilized a comprehensive tech stack including Keras, Sklearn, Scipy, and NumPy for model development, alongside Matplotlib and Plotly for effective data visualization
- • Conducted in-depth research into parameter optimization, reading several research papers and exploring different combinations of technical indicators
- • Experimented with prediction outputs, examining price, change percentage, and direction as potential options and fine-tuning minimum confidence thresholds
- • Investigated stock clustering techniques based on research paper to group similar companies for more robust model training, learning about winsorizing of outliers in the process
- """
+Explored evolutionary reinforcement learning, an advanced fusion of genetic algorithms and machine learning (ML). Genetic algorithms, inspired by biological evolution, were employed to generate randomized populations, selectively refining individuals based on heuristic evaluation for subsequent generations. ML models, particularly neural networks, allowed for complex decision-making processes.
+ • Leveraged the NEAT library to orchestrate the training of AI bots for complex games, including Flappy Bird, Pong, and 2048
+ • Delved into academic research papers pertaining to genetic programming and reinforcement learning, acquiring an in-depth understanding of fundamental principles
+ • Applied the min-max algorithm and conducted comprehensive heuristic explorations across various gaming scenarios
+"""
 
 lines = s.split("\n")
 for line in lines:
