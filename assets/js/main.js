d7963b1dd2a9f62d1309774b4696d1ccdde7ea6c
eric-zzhou <ericsuper@outlook.com>
2024-07-05 00:21:32 -0700

    Website edit for summer of freshmen year, resume section is still yet to be completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,39 @@
 /**
-* Template Name: Personal
-* Updated: Aug 30 2023 with Bootstrap v5.3.1
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+ * Template Name: Personal
+ * Updated: Aug 30 2023 with Bootstrap v5.3.1
+ * Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
 
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
    * Scrolls to an element with header offset
@@ -41,107 +41,116 @@
   const scrollto = (el) => {
     window.scrollTo({
       top: 0,
-      behavior: 'smooth'
-    })
-  }
+      behavior: "smooth",
+    });
+  };
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
   /**
    * Scroll with offset on links with a class name .scrollto
    */
-  on('click', '#navbar .nav-link', function(e) {
-    let section = select(this.hash)
-    if (section) {
-      e.preventDefault()
+  on(
+    "click",
+    "#navbar .nav-link",
+    function (e) {
+      let section = select(this.hash);
+      if (section) {
+        e.preventDefault();
 
-      let navbar = select('#navbar')
-      let header = select('#header')
-      let sections = select('section', true)
-      let navlinks = select('#navbar .nav-link', true)
+        let navbar = select("#navbar");
+        let header = select("#header");
+        let sections = select("section", true);
+        let navlinks = select("#navbar .nav-link", true);
 
-      navlinks.forEach((item) => {
-        item.classList.remove('active')
-      })
+        navlinks.forEach((item) => {
+          item.classList.remove("active");
+        });
 
-      this.classList.add('active')
+        this.classList.add("active");
 
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
-      }
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
 
-      if (this.hash == '#header') {
-        header.classList.remove('header-top')
-        sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
-        return;
-      }
-
-      if (!header.classList.contains('header-top')) {
-        header.classList.add('header-top')
-        setTimeout(function() {
+        if (this.hash == "#header") {
+          header.classList.remove("header-top");
           sections.forEach((item) => {
-            item.classList.remove('section-show')
-          })
-          section.classList.add('section-show')
+            item.classList.remove("section-show");
+          });
+          return;
+        }
 
-        }, 350);
-      } else {
-        sections.forEach((item) => {
-          item.classList.remove('section-show')
-        })
-        section.classList.add('section-show')
+        if (!header.classList.contains("header-top")) {
+          header.classList.add("header-top");
+          setTimeout(function () {
+            sections.forEach((item) => {
+              item.classList.remove("section-show");
+            });
+            section.classList.add("section-show");
+          }, 350);
+        } else {
+          sections.forEach((item) => {
+            item.classList.remove("section-show");
+          });
+          section.classList.add("section-show");
+        }
+
+        scrollto(this.hash);
       }
-
-      scrollto(this.hash)
-    }
-  }, true)
+    },
+    true
+  );
 
   /**
    * Activate/show sections on load with hash links
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
-      let initial_nav = select(window.location.hash)
+      let initial_nav = select(window.location.hash);
 
       if (initial_nav) {
-        let header = select('#header')
-        let navlinks = select('#navbar .nav-link', true)
+        let header = select("#header");
+        let navlinks = select("#navbar .nav-link", true);
 
-        header.classList.add('header-top')
+        header.classList.add("header-top");
 
         navlinks.forEach((item) => {
-          if (item.getAttribute('href') == window.location.hash) {
-            item.classList.add('active')
+          if (item.getAttribute("href") == window.location.hash) {
+            item.classList.add("active");
           } else {
-            item.classList.remove('active')
+            item.classList.remove("active");
           }
-        })
+        });
 
-        setTimeout(function() {
-          initial_nav.classList.add('section-show')
+        setTimeout(function () {
+          initial_nav.classList.add("section-show");
         }, 350);
 
-        scrollto(window.location.hash)
+        scrollto(window.location.hash);
       }
     }
     var curr = courseswiper.activeIndex - 3;
-    var slides = document.body.getElementsByClassName('course-swiper')[0].children;
+    var slides =
+      document.body.getElementsByClassName("course-swiper")[0].children;
     for (var i = 0; i < slides.length; i++) {
       var slide = slides[i];
-      var slideind = Number(slide.getAttribute('data-swiper-slide-index'));
-      if (slideind == curr || slideind == ((curr + 7) % 6) || slideind == ((curr + 8) % 6)) {
+      var slideind = Number(slide.getAttribute("data-swiper-slide-index"));
+      if (
+        slideind == curr ||
+        slideind == (curr + 10) % 9 ||
+        slideind == (curr + 11) % 9
+      ) {
         slide.style.opacity = "1";
       } else {
         slide.style.opacity = "0";
@@ -152,31 +161,31 @@
     // slides = document.body.getElementsByClassName('portfolio-details-slider')[0].children;
     // for (var i = 0; i < slides.length; i++) {
     //   var slide = slides[i];
-      
+
     // }
   });
 
   /**
    * Skills animation
    */
-  let skilsContent = select('.skills-content');
+  let skilsContent = select(".skills-content");
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
-      offset: '80%',
-      handler: function(direction) {
-        let progress = select('.progress .progress-bar', true);
+      offset: "80%",
+      handler: function (direction) {
+        let progress = select(".progress .progress-bar", true);
         progress.forEach((el) => {
-          el.style.width = el.getAttribute('aria-valuenow') + '%'
+          el.style.width = el.getAttribute("aria-valuenow") + "%";
         });
-      }
-    })
+      },
+    });
   }
 
   /**
    * courses slider
    */
-  let courseswiper = new Swiper('.courses-slider', {
+  let courseswiper = new Swiper(".courses-slider", {
     speed: 250,
     loop: true,
     grabCursor: true,
@@ -185,94 +194,105 @@
       prevEl: ".swiper-button-prev",
     },
     autoplay: false,
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
 
       1200: {
         slidesPerView: 3,
-        spaceBetween: 20
-      }
+        spaceBetween: 20,
+      },
     },
     keyboard: true,
   });
 
-  courseswiper.on('slideChange', function () {
-    var curr = (courseswiper.activeIndex + 3) % 6;
+  courseswiper.on("slideChange", function () {
+    console.log("active index:" + courseswiper.activeIndex);
+    var curr = (courseswiper.activeIndex + 6) % 9;
+    console.log("curr:" + curr);
 
-    var slides = document.body.getElementsByClassName('course-swiper')[0].children;
+    var slides =
+      document.body.getElementsByClassName("course-swiper")[0].children;
     for (var i = 0; i < slides.length; i++) {
       var slide = slides[i];
-      var slideind = Number(slide.getAttribute('data-swiper-slide-index'));
+      var slideind = Number(slide.getAttribute("data-swiper-slide-index"));
       // console.log(typeof(curr));
       // console.log(typeof(slideind));
       // console.log(curr + ", " + slideind + ", " + (slideind == curr || slideind == curr + 1 || slideind == curr + 2));
       // console.log(curr + ", " + (curr + 1) + ", " + (curr + 2));
       // console.log("");
-      if (slideind == curr || slideind == (curr + 1) % 6 || slideind == (curr + 2) % 6) {
+      if (
+        slideind == curr ||
+        slideind == (curr + 1) % 9 ||
+        slideind == (curr + 2) % 9
+      ) {
         slide.style.opacity = "1";
       } else {
         slide.style.opacity = "0";
       }
     }
-});
+  });
 
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
+        itemSelector: ".portfolio-item",
+        layoutMode: "fitRows",
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select("#portfolio-flters li", true);
 
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-      }, true);
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+        },
+        true
+      );
     }
-
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
+    selector: ".portfolio-lightbox",
   });
 
   /**
-   * Initiate portfolio details lightbox 
+   * Initiate portfolio details lightbox
    */
   const portfolioDetailsLightbox = GLightbox({
-    selector: '.portfolio-details-lightbox',
-    width: '90%',
-    height: '90vh'
+    selector: ".portfolio-details-lightbox",
+    width: "90%",
+    height: "90vh",
   });
 
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
+  new Swiper(".portfolio-details-slider", {
     speed: 250,
     loop: true,
     autoplay: false,
@@ -284,15 +304,14 @@
       prevEl: ".swiper-button-prev",
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
   });
 
   /**
-   * Initiate Pure Counter 
+   * Initiate Pure Counter
    */
   new PureCounter();
-
-})()
+})();
